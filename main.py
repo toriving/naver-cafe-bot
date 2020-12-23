ab3540de35026b578e71a5953f63595d133dd7c4
toriving <toriving@gmail.com>
2020-12-24 03:34:01 +0900

    Implement main

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from token import get_token
+from login import get_token
 from post import post_article
 
 
 def main():
     token = get_token()
     rescode = post_article(token)
-
+    print(rescode)
 
 if __name__ == "__main__":
     main()
